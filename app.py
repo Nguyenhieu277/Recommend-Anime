3269f2efa40fe793a3b5b8a4d2473c0f97beeb97
Nguyenhieu277 <nguyenphamtrunghieu277@gmail.com>
2024-12-04 10:02:32 +0700

    fix openai

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI, OpenAIError
 
+api_key = st.secrets["GITHUB_TOKEN"]
 class OpenAIClient:
     def __init__(self):
         load_dotenv()
-        self.token = os.getenv("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")
+        self.token = api_key
         self.endpoint = "https://models.inference.ai.azure.com"
         self.model_name = "gpt-4o-mini"
         try:
